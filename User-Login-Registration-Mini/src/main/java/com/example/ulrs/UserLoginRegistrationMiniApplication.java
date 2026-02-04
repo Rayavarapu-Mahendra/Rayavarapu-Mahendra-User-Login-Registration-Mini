fecc8d40bfb828139366188aecb5fdeb0897fd3a
Rayavarapu-Mahendra <rayavarapumahendra@zohomail.in>
2026-02-04 11:48:19 +0530

    Initial commit - Created project file

---
+++ User-Login-Registration-Mini/src/main/java/com/example/ulrs/UserLoginRegistrationMiniApplication.java
@@ -0,0 +1,13 @@
+package com.example.ulrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserLoginRegistrationMiniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserLoginRegistrationMiniApplication.class, args);
+	}
+
+}
---
+++ User-Login-Registration-Mini/src/test/java/com/example/ulrs/UserLoginRegistrationMiniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ulrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserLoginRegistrationMiniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
